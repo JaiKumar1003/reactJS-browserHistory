605c6323288ad1b4c71fcd39f4a181c3f2a002d4
JaiKumar1003 <jaijai100320@gmail.com>
2024-02-07 13:07:45 +0000

    Test Case Failed

---
+++ src/Components/HistoryEmptyItem/index.js
@@ -0,0 +1,11 @@
+import './index.css'
+
+const HistoryEmptyItem = () => {
+  return (
+    <li className="empty-history-card">
+      <p className="empty-history-name">There is no history to show</p>
+    </li>
+  )
+}
+
+export default HistoryEmptyItem
---
+++ src/Components/HistoryItem/index.js
@@ -0,0 +1,40 @@
+import './index.css'
+
+const HistoryItem = props => {
+  const {historyItem, deleteHistory} = props
+  const {timeAccessed, logoUrl, title, domainUrl, id} = historyItem
+
+  const onClickDelete = () => {
+    deleteHistory(id)
+  }
+
+  return (
+    <li className="history-list-item-card">
+      <div>
+        <p className="history-time">{timeAccessed}</p>
+      </div>
+      <div className="history-domain-delete-card">
+        <div className="history-logo-domain-card">
+          <img className="history-logo" src={logoUrl} alt="domain logo" />
+          <div className="history-domain-card">
+            <p className="history-name">{title}</p>
+            <p className="history-domain-url">{domainUrl}</p>
+          </div>
+        </div>
+        <button
+          id="delete"
+          type="button"
+          className="button"
+          onClick={onClickDelete}
+        >
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/delete-img.png"
+            alt="delete"
+          />
+        </button>
+      </div>
+    </li>
+  )
+}
+
+export default HistoryItem
